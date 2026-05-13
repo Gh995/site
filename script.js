76f968d02cdcd576a5f0d7a26099ac615637a724
Gh995 <mahdi.aladdab@gmail.com>
2026-05-13 03:02:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function updateClock() {
+    const now = new Date();
+    const h = String(now.getHours()).padStart(2, '0');
+    const m = String(now.getMinutes()).padStart(2, '0');
+    document.getElementById('clock').textContent = h + ':' + m  ;
+}
+ 
+updateClock();
+setInterval(updateClock, 1000);
